71f38009de40132218869d9b393b1eee05d22f46
Brian Hogan <brianhogan@napcs.com>
2009-11-15 01:37:59 -0600

    update selenium to fix bugs

--- features/support/selenium.rb
+++ features/support/selenium.rb
@@ -8,13 +8,29 @@ Cucumber::Rails::World.use_transactional_fixtures = false
 require 'database_cleaner'
 DatabaseCleaner.clean_with :truncation
 DatabaseCleaner.strategy = :truncation
-# Before do
-#   DatabaseCleaner.start
-# end
-# After do
-#   DatabaseCleaner.clean
-# end
+Before do
+  DatabaseCleaner.start
+end
+After do
+  DatabaseCleaner.clean
+end
 
 
+module Webrat
+  class SeleniumSession
+    
+    ["click_link", "click_button"].each do |method|
+      class_eval <<-EOF
+        alias :old_#{method} :#{method}
+        def #{method}(*args)
+          old_#{method}(args)
+          warn "waiting for the page to load..."
+          selenium.wait_for_page_to_load
+        end
+      EOF
+    end
 
+  end
+  
+end
 
